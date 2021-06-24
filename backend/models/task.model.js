f10a930a79dfd19945d2fc3ce10ec71bea3615fa
asacela <alec.asatoorian@embodied.me>
2021-06-23 18:30:07 -0700

    update task model

---
+++ backend/models/task.model.js
@@ -0,0 +1,45 @@
+/*
+	This file establishes the
+	schemas for the databases
+	used in this project.
+*/
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+/* 
+	Describes schema for tasks db.
+	Used for storing task info that
+	goes into the day schema.
+*/
+const taskSchema = new Schema({
+	
+  name: {
+  	type: String,
+  	required: true
+  },
+  points: {
+  	type: Number,
+  	required: true
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  complete: {
+  	type: Boolean,
+  	required: true
+  },
+  backlog: {
+    type: Boolean,
+    required: true
+  },
+}, {
+	timestamps: true,
+});
+
+
+/*
+  Export the schema.
+*/
+const Task = mongoose.model('Task', taskSchema);
+module.exports = Task;
